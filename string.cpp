20cbaadb75541588c2f3ecccb8659a5be403426e
Dappstr <87837549+Dappstr@users.noreply.github.com>
2023-02-24 10:25:15 -0800

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <memory>
+#include <cstring>
+#include <utility>
 
 class String
 {
@@ -30,6 +29,12 @@ class String
                 memcpy(m_buffer, src.m_buffer, m_size);
                 m_buffer[m_size+1] = '\0';
         }
+
+        /*
+        Possibly reserved for future implementation.
+        String(std::initializer_list<const char*> lst) {
+
+        }*/
 
         String(String&& src) noexcept
             :m_size(std::move(src.m_size)), m_buffer(std::move(src.m_buffer)) {
@@ -68,17 +73,30 @@ class String
             return strlen(m_buffer);
         }
 
-        friend std::ostream& operator<<(std::ostream& out, const String& str);
+        friend std::ostream& operator<<(std::ostream& out, const String& str) {
+            out << str.m_buffer;
+            return out;
+        }
+
         char& operator[](size_t indx) noexcept(noexcept(indx >= 0 && indx < this->size())) {
             return m_buffer[indx];
+        }
+
+        operator char*() const {
+            return m_buffer;
+        }
+
+        String operator+(const String& rhs) {
+            auto newSize = this->m_size + rhs.m_size;
+            char combinedString[newSize+1];
+            strcpy(combinedString, this->m_buffer);
+            memcpy(combinedString + this->m_size, rhs.m_buffer, rhs.m_size);
+            String newString(combinedString);
+            newString.m_buffer[newSize+1] = '\0';
+            return newString;
         }
 
         ~String() {
             delete[] m_buffer;
         }
 };
-
-std::ostream& operator<<(std::ostream& out, const String& str) {
-    out << str.m_buffer;
-    return out;
-}
