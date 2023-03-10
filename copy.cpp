95edc01145720b5b08f0169c17d2b22d6036cf11
Dappstr <87837549+Dappstr@users.noreply.github.com>
2023-03-09 21:52:44 -0800

    Create copy.cpp

---
+++ copy.cpp
@@ -0,0 +1,7 @@
+void copy(const auto& srcBegin, const auto& srcEnd, auto& dstBegin) {
+    /*TODO
+        Get size of source
+        Get size of destination
+        Handle deep copying? Do users prefer shallow copy? Possibly check if destination underlying type is the non-pointer type of source and then do deep copy
+        */
+}
