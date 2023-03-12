1ac1ac80192fb960a16807358bbb19e0f3ddd0ca
Dappstr <87837549+Dappstr@users.noreply.github.com>
2023-03-12 14:08:07 -0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,9 @@
+/*
+Manual implementation of a stack without the use of a container such as std::vector as a buffer
+WIP
+*/
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <memory>
 
 template <typename T>
 concept acceptableType =
@@ -10,103 +12,96 @@ concept acceptableType =
                         std::is_copy_assignable_v<std::decay_t<T>> &&
                         std::is_move_assignable_v<std::decay_t<T>>;
 
-template <acceptableType T>
-//requires std::is_move_constructible_v<std::decay_t<T>> && std::is_copy_constructible_v<std::decay_t<T>>
+template <acceptableType T, size_t N = 0>
 class Stack
 {
     private:
-        std::vector<T> m_stack{};
+        //std::vector<T> m_stack{};
+        T* m_buffer {};
+        size_t m_size{N};
 
     protected:
-        void insertElement(const T& val) const {
-            m_stack.push_back(val);
+    void insertElement(const T& val) {
+            ++m_size;
+            T* tempBuff = new T[m_size];
+
+            memcpy(tempBuff, m_buffer, m_size-1);
+            tempBuff[m_size-1] = val;
+
+            delete[] m_buffer;
+            m_buffer = new T[m_size];
+
+            //memcpy(m_buffer, tempBuff, m_size); Not working for some reason
+            for(size_t i = 0; i < m_size; ++i) {
+                m_buffer[i] = std::move(tempBuff[i]);
+            }
+            delete[] tempBuff;
         }
 
         void insertElement(T&& val) {
-            m_stack.push_back(std::move(val));
+            ++m_size;
+            T* tempBuff = new T[m_size];
+
+            memcpy(tempBuff, m_buffer, m_size-1);
+            tempBuff[m_size-1] = std::move(val);
+
+            delete[] m_buffer;
+            m_buffer = new T[m_size];
+
+            //memcpy(m_buffer, tempBuff, m_size); Not working for some reason
+            for(size_t i = 0; i < m_size; ++i) {
+                m_buffer[i] = std::move(tempBuff[i]);
+            }
+            delete[] tempBuff;
         }
 
     public:
+        //Default allocation for a stack of type T
         Stack() = default;
-        Stack(std::initializer_list<T> list) {
-            std::for_each(list.begin(), list.end(), 
-            [&list, this](T val) mutable {
-                m_stack.push_back(std::move(val));
-            });
-        }
+
         Stack(T val) {
-            m_stack.push_back(val);
+            assert(N > 0);
+            m_buffer = new T[m_size];
+            m_buffer[m_size-1] = val;
         }
 
-        Stack(const Stack& src) {
-            std::copy(src.m_stack.begin(), src.m_stack.end(), std::back_inserter(m_stack));
-        }
+        size_t size() { return m_size; }
+
+
+        //default for now, implementing later    
+        Stack(const Stack& src) = default;
+
+        /*
+        Finish later
+        Stack(const Stack& src) noexcept(noexcept(m_size )){
+        }*/
+
+         //Default for now, implementing later    
+        Stack(Stack&& src) = default;
+
+        //Operators
+        //Default for now, implementing later
+        Stack& operator=(const Stack& src) = default;
+
+        /*
         Stack& operator=(const Stack& src) {
             std::copy(src.m_stack.begin(), src.m_stack.end(), std::back_inserter(m_stack));
-        }
+        }*/
 
-        Stack(Stack&& src) = default;
+        //Default for now, implementing later
         Stack& operator=(Stack&& src) = default;
 
-        inline size_t size() noexcept(noexcept(m_stack.size() >= 0)) { 
-            return m_stack.size();
-        }
-
-        void push(T val) {
-            insertElement(std::forward<T>(val));
-        }
-
-        T pop() {
-            assert(m_stack.size() > 0);
-            T value = m_stack.back();
-            m_stack.pop_back();
-            return value;
-        }
-
-        const T& top() const {
-            return m_stack.back();
-        }
-
-        bool empty() {
-            return m_stack.empty();
-        }
-
-        T operator[](size_t index) noexcept(noexcept(index >= 0)) {
-            return m_stack.at(index);
-        }
-
-        const T operator[](size_t index) const noexcept(noexcept(index >= 0)) {
-            return m_stack.at(index);
-        }
-
-        friend std::ostream& operator<<(std::ostream& out, Stack<T>& stack) {
-            for(size_t i = 0; i < static_cast<size_t>(stack.size()); ++i) {
-                out << stack[i] << ' ';
+        friend std::ostream& operator<<(std::ostream& out, C<T, N>& c) {
+            for(size_t i = 0; i < c.m_size; ++i) {
+                out << '[';
+                if(i == c.m_size - 1) { out << c.m_buffer[i] << ']'; }
+                else { out << c.m_buffer[i] << "], "; }
             }
             return out;
         }
 
-        void operator<<(const Stack& stack) const {
-            for(size_t i = 0; i < static_cast<size_t>(m_stack.size()); ++i) {
-                std::cout << stack[i] << ' ';
-            }
+        ~Stack() {
+            delete[] m_buffer;
+            m_size = 0;
         }
-
-        bool operator==(Stack<T>& rhs) {
-            return std::equal(m_stack.begin(), m_stack.end(), rhs.m_stack.begin(), rhs.m_stack.end());
-            //assert(m_stack.size() == rhs.size());
-
-            /*
-            Manual way
-            if(m_stack.size() != rhs.size()) { return false;}
-
-            for(size_t i = 0; i < m_stack.size(); ++i) {
-                if(m_stack[i] != rhs[i]) { return false;}
-            }
-            
-            return true;
-            */
-        }
-
-        ~Stack() = default;
 };
